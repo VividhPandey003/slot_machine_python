5db9bfedad94523a44b577a2541c5119af478b5a
Vividh Pandey <vividh.pandey.13@gmail.com>
2023-01-08 17:40:21 +0530

    Update main.py

--- main.py
+++ main.py
@@ -41,9 +41,10 @@ def print_slot_machine(columns):
     for row in range(len(columns[0])):
         for i, column in enumerate(columns):
             if i != len(columns)-1:  # maximum index we have is len(cols)-1
-                print(column[row], end="  |  ")
+                print(column[row], end=" | ")
             else:
                 print(column[row], end="")
+        print()
 
 
 def deposit():
